bba450431f1c01a581432126aa35139f63eca59a
liguoliang777 <157308001@qq.com>
2018-06-05 16:02:24 +0800

    1.项目列表

--- Task/src/main/java/com/android/taskallo/project/view/CardDetailActivity.java
+++ Task/src/main/java/com/android/taskallo/project/view/CardDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
@@ -15,8 +16,10 @@ import com.android.taskallo.core.utils.KeyConstant;
 public class CardDetailActivity extends BaseFgActivity implements PopupMenu
         .OnMenuItemClickListener {
     private Button mTop_Left_Finished_BT, mTop_Left_Delete_BT, mTopEditSaveBt;
-    private String mCardTitle, mCardId;
+    private String mCardTitle = "";
+    private String mCardId = "";
     private CardDetailActivity context;
+    private EditText mCardTitleEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,14 @@ public class CardDetailActivity extends BaseFgActivity implements PopupMenu
         context = this;
         Intent intent = getIntent();
         mCardTitle = intent.getStringExtra(KeyConstant.cardTitle);
+        mCardTitle = mCardTitle == null ? "" : mCardTitle;
         mCardId = intent.getStringExtra(KeyConstant.cardId);
 
         mTop_Left_Finished_BT = (Button) findViewById(R.id.top_left_finish_bt);
         mTop_Left_Delete_BT = (Button) findViewById(R.id.top_left_delete_bt);
+        mCardTitleEt = (EditText) findViewById(R.id.card_title_et);
+        mCardTitleEt.setText(mCardTitle);
+        mCardTitleEt.setSelection(mCardTitle.length());
 
         mTop_Left_Finished_BT.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,22 +44,17 @@ public class CardDetailActivity extends BaseFgActivity implements PopupMenu
                 finish();
             }
         });
-        mTop_Left_Finished_BT.setText("卡片");
 
         mTopEditSaveBt = (Button) findViewById(R.id.edit_right_save_bt);
         //mTop_Right_SaveTv.setVisibility(View.VISIBLE);
 
     }
 
-
+    //顶部弹窗
     public void showCardPopupWindow(View v) {
-        //实例化一个弹出式菜单，传入上下文和控件
         PopupMenu popupMenu = new PopupMenu(context, v);
-        //根据菜单填充器获得菜单的布局
         popupMenu.getMenuInflater().inflate(R.menu.card_menu, popupMenu.getMenu());
-        //设置菜单的点击事件
         popupMenu.setOnMenuItemClickListener(this);
-        //显示菜单
         popupMenu.show();
     }
 
@@ -70,4 +72,24 @@ public class CardDetailActivity extends BaseFgActivity implements PopupMenu
         }
         return true;
     }
+
+    //添加子任务的item
+    public void onSubTaskAddBtClick(View view) {
+
+    }
+
+/*    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager imm= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (getCurrentFocus() != null) {
+                if (getCurrentFocus().getWindowToken() != null) {
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+            }
+        }
+        return super.onTouchEvent(event);
+
+    }*/
 }
