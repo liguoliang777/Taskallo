d85d54c9696b5adaa462a607fd90d41ddbd591fc
liguoliang777 <157308001@qq.com>
2018-07-10 15:36:55 +0800

    1.文件图片列表

--- Task/src/main/java/com/android/taskallo/project/view/CardDetailActivity.java
+++ Task/src/main/java/com/android/taskallo/project/view/CardDetailActivity.java
@@ -257,9 +257,11 @@ public class CardDetailActivity extends CommonBaseActivity implements PopupMenu
 
         mGridView = (GridView) findViewById(R.id.horizontal_gridview);
 
-
-        /*String fileUrl = "http://oss.fanglenet.cn/upload/1531127053591.jpg";
+  /*      String fileUrl = "http://oss.fanglenet.cn/upload/1531127053591.jpg";
         FileListInfo fileListInfo = new FileListInfo("0", "112", fileUrl, 0, 0);
+        mFileListData.add(fileListInfo);
+        mFileListData.add(fileListInfo);
+        mFileListData.add(fileListInfo);
         mFileListData.add(fileListInfo);*/
 
         fileListAdapter = new FileListAdapter(context, mFileListData);
